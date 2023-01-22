8fdf33206e4b8c43521a29bc3f75beda6d74a9e2
Kaputchino <andreasmulard@gmail.com>
2023-01-22 13:17:19 +0100

    randomize

--- code/java/convert/src/main/java/org/example/Party.java
+++ code/java/convert/src/main/java/org/example/Party.java
@@ -8,11 +8,14 @@ public class Party {
 
     public Party(String nom) {
         this.nom = nom;
+        this.color = setColorRandomly();
+    }
+
+    private String setColorRandomly() {
         Random obj = new Random();
         int rand_num = obj.nextInt(0xffffff + 1);
 // format it as hexadecimal string and print
-        color = String.format("#%06x", rand_num);
-
+        return String.format("#%06x", rand_num);
     }
 
     @Override
